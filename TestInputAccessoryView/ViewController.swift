3fdb8009cbd718f2a8c502f8c71279aa2ceb98ff
Alex Huang <alexhuangtung@gmail.com>
2019-11-15 17:15:32 +0800

    Initial Commit

---
+++ TestInputAccessoryView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestInputAccessoryView
+//
+//  Created by Alex Huang on 2019/11/15.
+//  Copyright © 2019 Alex Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
